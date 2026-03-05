2009055457e68d6b7df2d5769e843a9c0a0cea70
LuizHBF <145623728+LuizHBF@users.noreply.github.com>
2026-03-05 13:17:29 +0000

    Perfumaria

--- src/tests/App.test.jsx
+++ src/tests/App.test.jsx
@@ -1,7 +1,16 @@
 import { render, screen } from "@testing-library/react";
 import Home from "../pages/index";
+
 describe("Página inicial", () => {
- it("deve renderizar o título principal", () => {
- render(<Home />);
- expect(screen.getByText("Bem-vindo à Pousada Pedra Furada")).toBeInTheDocument();  });
+    it("deve renderizar o título principal", () => {
+        const { container } = render(<Home />);
+        console.log(container.innerHTML); // 踬踭踮踯 mostra o HTML renderizado
+        expect(
+            screen.getByText("Bem-vindo à Pousada Pedra Furada")
+        ).toBeInTheDocument();
+    w});
 });
+
+<main>
+<h1>Bem-vindo à Pousada Pedra Furada</h1>
+</main>
